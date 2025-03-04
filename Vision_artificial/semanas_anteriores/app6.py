1baa849c66d8a0a4617519c0cb92bf5aa95935fe
Kasuteryon <eduardocastellon2002@gmail.com>
2025-03-04 16:20:41 -0600

    march 4 class

--- Vision_artificial/semanas_anteriores/app6.py
+++ Vision_artificial/semanas_anteriores/app6.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 def runSeparation():
-    image = cv2.imread("./assets/me2.jpg")
+    image = cv2.imread("../../assets/cacao.jpg")
 
     secondary_image = image.copy()
    
@@ -26,18 +26,24 @@ def runSeparation():
     m,n = np.shape(gray_image)
     for i in range(0,m):
         for j in range (0,n):
-            if gray_image[i,j] < 100:
+            if gray_image[i,j] < 127:
                 gray_image[i,j] = 255
             else:
                 gray_image[i,j] = 0
 
     fg = plt.figure(figsize=(10,10))
 
-    sub = fg.add_subplot(1, 2, 1)
+    sub = fg.add_subplot(1, 1, 1)
     histogram = cv2.calcHist([np.transpose(gray_image)],[0],None,[255],[0,255]) 
-    sub.plot(histogram)
-    sub = fg.add_subplot(1, 2, 2)
-    sub.imshow(np.invert(gray_image), cmap='gray')
+    # sub.plot(histogram)
+    # sub = fg.add_subplot(1, 2, 2)
+    # sub.imshow(np.invert(gray_image), cmap='gray')
+    # SOBEL
+
+    Sx = cv2.Sobel(gray_image, cv2.CV_64F, 1, 0, ksize=11)
+    Sy = cv2.Sobel(gray_image, cv2.CV_64F, 0, 1, ksize=11)
+    sobel_mag = np.sqrt(Sx**2 + Sy **2)
+    sub.imshow(sobel_mag)
     # sub.imshow(gray_image)
     plt.show()
 
